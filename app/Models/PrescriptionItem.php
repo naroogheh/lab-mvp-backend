1d44b4af0565b3f140d86b4636d1783747c18b1f
Javad Naroogheh <j.naroogheh@gmail.com>
2026-08-31 10:09:26 +0330

    Enable backend model factories for tests

--- app/Models/PrescriptionItem.php
+++ app/Models/PrescriptionItem.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PrescriptionItem extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'prescription_id',
         'lab_test_id',

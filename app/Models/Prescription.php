409ff1cbe2fc7cc98a5c30a8f8243045b73429a7
Javad Naroogheh <j.naroogheh@gmail.com>
2026-08-31 10:09:24 +0330

    Enable backend model factories for tests

--- app/Models/Prescription.php
+++ app/Models/Prescription.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 use App\Enums\PrescriptionStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Prescription extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'tracking_number',
         'image_path',
